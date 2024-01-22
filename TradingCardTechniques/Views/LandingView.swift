3192eee7b766488b5b16e45d812a0f346b1264a8
Russell Gordon <rgordon@lcs.on.ca>
2024-01-22 07:34:19 -0500

    Added pointers to pages in SwiftUI Views Mastery.

--- TradingCardTechniques/Views/LandingView.swift
+++ TradingCardTechniques/Views/LandingView.swift
@@ -14,19 +14,40 @@ struct LandingView: View {
                 NavigationLink {
                     RotationEffectView()
                 } label: {
-                    Label(
-                        "Rotation",
-                        systemImage: "rotate.right"
-                    )
+                    HStack {
+                        
+                        Image(systemName: "rotate.right")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25)
+                            .padding(.trailing, 15)
+                        
+                        VStack {
+                            VStack(alignment: .leading) {
+                                Text("Rotation")
+                                    .font(.headline)
+                                Text("Effect Modifiers, p. 857")
+                            }
+                        }
+                        
+                    }
                 }
                 
                 NavigationLink {
                     LayeringTabView()
                 } label: {
-                    Label(
-                        "Layers",
-                        systemImage: "square.3.layers.3d"
-                    )
+                    
+                    Image(systemName: "square.3.layers.3d")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 25)
+                        .padding(.trailing, 15)
+
+                    VStack(alignment: .leading) {
+                        Text("Layers")
+                            .font(.headline)
+                        Text("SwiftUI Basics, p. 46")
+                    }
                 }
 
 
